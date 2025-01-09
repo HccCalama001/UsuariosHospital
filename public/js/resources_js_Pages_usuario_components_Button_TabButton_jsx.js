db16484afe9ec90b377631062346fdeb21cebf87
HccCalama001 <hcc.calamasoporte@gmail.com>
2025-01-09 10:34:42 -0300

    09012025v2

---
+++ public/js/resources_js_Pages_usuario_components_Button_TabButton_jsx.js
@@ -0,0 +1,13 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_Pages_usuario_components_Button_TabButton_jsx"],{
+
+/***/ "./resources/js/Pages/usuario/components/Button/TabButton.jsx":
+/*!********************************************************************!*\
+  !*** ./resources/js/Pages/usuario/components/Button/TabButton.jsx ***!
+  \********************************************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+}]);
