4538c3ffb4c61bed4384ae9ee897433a7ba63fdf
HccCalama001 <hcc.calamasoporte@gmail.com>
2025-01-28 14:08:38 -0300

    28012025v4

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,28 +6,55 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * These middleware are run during every request to your application.
+     *
+     * @var array<int, class-string|string>
+     */
     protected $middleware = [
         \App\Http\Middleware\TrustProxies::class,
-        \Fruitcake\Cors\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class, // Manejo de CORS
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array<string, array<int, class-string|string>>
+     */
     protected $middlewareGroups = [
         'web' => [
+            \App\Http\Middleware\EncryptCookies::class, // Para encriptar cookies
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class, // Para manejar cookies
+            \Illuminate\Session\Middleware\StartSession::class, // Iniciar sesiones
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class, // Verificar tokens CSRF
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
-            \Fruitcake\Cors\HandleCors::class,
+            \Fruitcake\Cors\HandleCors::class, // Manejo de CORS en API
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Session\Middleware\StartSession::class, // Para manejar sesiones en rutas API
+            \App\Http\Middleware\EncryptCookies::class, // Encriptar cookies en rutas API
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             'throttle:api',
-            \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
+            \Tymon\JWTAuth\Http\Middleware\Authenticate::class, // Middleware para autenticar JWT
         ],
     ];
 
+    /**
+     * The application's route middleware.
+     *
+     * These middleware may be assigned to groups or used individually.
+     *
+     * @var array<string, class-string|string>
+     */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'temporaryToken' => \App\Http\Middleware\TemporaryTokenMiddleware::class,
